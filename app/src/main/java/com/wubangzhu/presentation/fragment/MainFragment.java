6e09e078b5cff6a6bd563b9a883ec4699439faae
jjstifler <jj.cong.zhou@gmail.com>
2018-11-02 16:20:56 +0800

    add barrage

--- app/src/main/java/com/wubangzhu/presentation/fragment/MainFragment.java
+++ app/src/main/java/com/wubangzhu/presentation/fragment/MainFragment.java
@@ -6,19 +6,34 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.kd.easybarrage.Barrage;
+import com.kd.easybarrage.BarrageView;
 import com.wubangzhu.R;
+import com.wubangzhu.presentation.activity.MainActivity;
 import com.wubangzhu.presentation.widgets.GlideImageLoader;
 import com.wubangzhu.util.ShareKeys;
 import com.youth.banner.Banner;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainFragment extends BaseFragment {
 
     @BindView(R.id.banber_main)
     Banner banner;
+    @BindView(R.id.barrageView)
+    BarrageView barrageView;
+    @BindView(R.id.btn_gotomall)
+    Button gotoMall;
+    @BindView(R.id.btn_gotogame)
+    Button gotoGame;
+
 
     @Nullable
     @Override
@@ -32,5 +47,15 @@ public class MainFragment extends BaseFragment {
         banner.setImageLoader(new GlideImageLoader());
         banner.setImages(ShareKeys.getImages());
         banner.start();
+        barrageView.addBarrage(new Barrage("恭喜XXX猜中iPhone XS Max！"));
+    }
+    @OnClick({R.id.btn_gotomall,R.id.btn_gotogame})
+    void onclick(View v){
+        if(v == gotoMall){
+            ToastUtils.showShort("购物区");
+            updateFragment(new AllGoodsFragment(),1);
+        }else{
+            ToastUtils.showShort("娱乐区");
+        }
     }
 }
