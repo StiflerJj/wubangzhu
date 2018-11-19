4f3917da7c69966c27f7ced76c9e9ecc26aa4310
jjstifler <jj.cong.zhou@gmail.com>
2018-11-19 22:10:56 +0800

    url

--- app/src/main/java/com/wubangzhu/domain/http/URL.java
+++ app/src/main/java/com/wubangzhu/domain/http/URL.java
@@ -1,5 +1,5 @@
 package com.wubangzhu.domain.http;
 
 public class URL {
-    public static final String HOST ="http://192.168.0.102:9090";//http://139.224.69.42/kbuaservice
+    public static final String HOST ="http://39.98.62.18:9999/guess/";//http://139.224.69.42/kbuaservice
 }
