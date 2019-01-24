202d97b4463ed1c565930e9de99245eab9169a95
jjstifler <jj.cong.zhou@gmail.com>
2019-01-24 17:03:47 +0800

    add barrage

--- app/src/main/java/com/wubangzhu/domain/http/URL.java
+++ app/src/main/java/com/wubangzhu/domain/http/URL.java
@@ -2,6 +2,6 @@ package com.wubangzhu.domain.http;
 
 public class URL {
 //    public static final String HOST ="http://192.168.0.102:9090/";
-    public static final String HOST ="http://39.98.62.18:9999/guess/";
+    public static final String HOST ="http://39.98.65.213:9999/guess/";
 }
 
